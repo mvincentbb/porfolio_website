ba332a500eeebfa13e1c5a0ddcb80078066df5fe
vincent <vincent.bakpatina@ipnetinstitute.com>
2022-06-12 07:47:58 +0000

    add reatjs and tailwind js background and header

---
+++ src/components/Me/Me.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import LOGO from '../../assets/Logo.svg'
+
+const Me =  () =>{
+
+    return ( 
+        <nav className='relative container mx-auto px-10'>
+            <div className="flex items-center justify-between">
+                <div className='pt-4'>
+                    <img src={LOGO}  alt="" /> 
+                </div>
+                <div className="hidden md:flex space-x-60">
+                    <a href="#" className="hover:text-blue  hover:font-bold">BLOG</a>
+                    <a href="#"className="hover:text-blue hover:font-bold">ABOUT</a>
+                </div>
+            </div>
+        </nav>
+
+    );
+}
+
+export default  Me;
