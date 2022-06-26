be13b5b343bf3310931fa6e789cfc3f00e115f35
Christophe ASSEKOUDA <christophe1999da@gmail.com>
2022-06-26 08:46:27 +0000

    I centered the twitter icon vertically

--- src/components/menu/Nav.js
+++ src/components/menu/Nav.js
@@ -13,35 +13,63 @@ import TwiterIcon from '../../assets/twiter_icon_active.svg'
 import TwiterIconInactive from '../../assets/twiter_icon_inactive.svg'
 
 
-
 const Nav = () => {
-    const [activeNav, setActiveNav] = useState("#")
-    return (
-        <nav className="bg-[#FFFFFF] bg-opacity-30 rounded-3xl w-max px-4 py-2 left-1/2 -translate-x-1/2 bottom-2 flex gap-4 fixed backdrop-blur-lg ">
-            <a href="https://www.facebook.com/vincent.bakpa" target="_blank" rel="noopener noreferrer" >
-                <img className="w-10 md:w-auto"  alt="Meta" onMouseLeave={()=> setActiveNav("#")} onMouseOver= {()=> setActiveNav("#meta")} src= { activeNav === "#meta" ? MetaIcon : MetaIconInActive}/>
-            </a>
-
-            <a href="https://github.com/mvincentbb" target="_blank" rel="noopener noreferrer">
-                <img className="w-10 md:w-auto"  alt="Github" onMouseLeave={()=> setActiveNav("#")} onMouseOver= {()=> setActiveNav("#github")} src= { activeNav === "#github" ? GithubIcon : GithubIconInactive}/>
-            </a>
-            <a href="https://vincentdepaul.notion.site/Projects-32cc2194c7f542a0998bd28b9897f981" target="_blank" rel="noopener noreferrer">
-                <img className="w-10 md:w-auto"  alt="Project" onMouseLeave={()=> setActiveNav("#")} onMouseOver= {()=> setActiveNav("#project")} src= { activeNav === "#project" ? ProjectIcon : ProjectIconInActive}/>
-
-            </a>
-            <a href="https://www.linkedin.com/in/vincent-bakpatina" target="_blank" rel="noopener noreferrer">
-                <img className="w-10 md:w-auto" alt="Project" onMouseLeave={()=> setActiveNav("#")} onMouseOver= {()=> setActiveNav("#linkledln")} src= { activeNav === "#linkledln" ? LinkledlnIcon : LinkledlnIconInActive}/>
-            </a>
-            <a href="https://twitter.com/BakpatinaP" target="_blank" rel="noopener noreferrer">
-
-                <img className="w-10 md:w-auto"
-                     alt="Twitter"
-                     onMouseLeave={()=> setActiveNav("#")}
-                     onMouseOver= {()=> setActiveNav("#twiter")}
-                     src= { activeNav === "#twiter" ? TwiterIcon :TwiterIconInactive}/>
-            </a>
-        </nav>
-    );
+	const [activeNav, setActiveNav] = useState("#")
+ 
+	return (
+		<nav
+			className="bg-[#FFFFFF] bg-opacity-30 rounded-3xl w-max px-4 py-2 left-1/2 -translate-x-1/2 bottom-2 flex gap-4 fixed backdrop-blur-lg ">
+			<a href="https://www.facebook.com/vincent.bakpa" target="_blank" rel="noopener noreferrer">
+				<img
+					className="w-10 md:w-auto"
+					alt="Meta"
+					onMouseLeave={() => setActiveNav("#")}
+					onMouseOver={() => setActiveNav("#meta")}
+					src={activeNav === "#meta" ? MetaIcon : MetaIconInActive}
+				/>
+			</a>
+			
+			<a href="https://github.com/mvincentbb" target="_blank" rel="noopener noreferrer">
+				<img
+					className="w-10 md:w-auto"
+					alt="Github"
+					onMouseLeave={() => setActiveNav("#")}
+					onMouseOver={() => setActiveNav("#github")}
+					src={activeNav === "#github" ? GithubIcon : GithubIconInactive}
+				/>
+			</a>
+			
+			<a href="https://vincentdepaul.notion.site/Projects-32cc2194c7f542a0998bd28b9897f981" target="_blank" rel="noopener noreferrer">
+				<img
+					className="w-10 md:w-auto"
+					alt="Project"
+					onMouseLeave={() => setActiveNav("#")}
+					onMouseOver={() => setActiveNav("#project")}
+					src={activeNav === "#project" ? ProjectIcon : ProjectIconInActive}
+				/>
+			</a>
+			
+			<a href="https://www.linkedin.com/in/vincent-bakpatina" target="_blank" rel="noopener noreferrer">
+				<img
+                    className="w-10 md:w-auto"
+                    alt="Project"
+                    onMouseLeave={() => setActiveNav("#")}
+                    onMouseOver={() => setActiveNav("#linkledln")}
+                    src={activeNav === "#linkledln" ? LinkledlnIcon : LinkledlnIconInActive}
+                />
+			</a>
+			
+			<a className="flex items-center" href="https://twitter.com/BakpatinaP" target="_blank" rel="noopener noreferrer">
+				<img
+                    className="w-10 md:w-auto"
+                    alt="Twitter"
+                    onMouseLeave={() => setActiveNav("#")}
+                    onMouseOver={() => setActiveNav("#twiter")}
+                    src={activeNav === "#twiter" ? TwiterIcon : TwiterIconInactive}
+                />
+			</a>
+		</nav>
+	);
 }
 
-export default Nav ;
+export default Nav;
