97061046c9be6c62d609b2607f3126a9175939d1
Christophe ASSEKOUDA <christophe1999da@gmail.com>
2022-06-26 12:37:18 +0000

    link on the logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,9 @@ const Header =  () =>{
         <nav className='relative container mx-auto px-10'>
             <div className="flex items-center justify-between">
                 <div className='pt-4'>
-                    <img src={LOGO}  alt="" />
+                    <a href="/">
+                        <img src={LOGO}  alt="" />
+                    </a>
                 </div>
                 <div className="hidden md:flex space-x-60">
                     <a href="#" className="hover:text-blue  hover:font-bold">BLOG</a>
