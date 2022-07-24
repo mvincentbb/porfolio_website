eabc3eef9cb6fb3b4f16173b2e545ec52175dfd4
vincent <vincent.bakpatina@ipnetinstitute.com>
2022-07-24 18:22:34 +0000

    responsive menu add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import LOGO from '../../assets/Logo.svg' 
+import React, {useState} from "react"
+import LOGO from "../../assets/Logo.svg"
+
+
 // eslint-disable-next-line import/no-unresolved
 // import LOGO from './assets/Logo.svg'
 
 
-const Header =  () =>{
 
-    return (
-        <nav className='relative container mx-auto px-10'>
-            <div className="flex items-center justify-between">
-                <div className='pt-4'>
-                    <a href="/">
-                        <img src={LOGO}  alt="" />
-                    </a>
-                </div>
-                <div className="hidden md:flex space-x-60">
-                    <a href="#" className="hover:text-blue  hover:font-bold">BLOG</a>
-                    <a href="#" className="hover:text-blue hover:font-bold">ABOUT</a>
-                </div>
-            </div>
-        </nav>
+const Header = () => {
+    const [hamburgerMenuSelected, setHamburgerMenuSelected] = useState(false)
+  return (
+    <nav className="relative container mx-auto px-10">
+      <div className="flex items-center justify-between">
+        {/* Logo */}
+        <div className="pt-4">
+          <a href="/">
+            <img src={LOGO} alt="" />
+          </a>
+        </div>
+        {/* Menu Item */}
+        <div className="hidden md:flex space-x-60">
+          <a href="#" className="hover:text-blue  hover:font-bold">
+            BLOG
+          </a>
+          <a href="#" className="hover:text-blue hover:font-bold">
+            ABOUT
+          </a>
+        </div>
 
-    );
+            {/*  Hamburger Icon */}
+          <button
+            id="menu-btn"
+            onClick={( )=> setHamburgerMenuSelected(!hamburgerMenuSelected)}
+            className={ hamburgerMenuSelected === true ? `open
+                        block hamburger md:hidden focus:outline-none`  : `block hamburger md:hidden focus:outline-none` } 
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+         
+      </div>
+         {/* Mobile Menu */}
+         <div className="md:hidden">
+          <div id="menu" 
+
+        //   absolute flex-col items-center hidden self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md
+          className=  { hamburgerMenuSelected === true ?
+            `absolute flex flex-col 
+            items-center   
+           self-end -mt-4 py-6 px-8 space-y-6 font-bold bg-white sm:w-auto 
+           sm:self-center  right-12 drop-shadow-lg`:
+            `hidden absolute flex flex-col 
+           items-center   
+          self-end -mt-4 py-6 px-8 space-y-6 font-bold bg-white sm:w-auto 
+          sm:self-center  right-12 drop-shadow-lg`}>
+            <a href="#">BLOG</a>
+            <a href="#">ABOUT</a>
+          </div>
+        </div>
+    </nav>
+  )
 }
 
-export default  Header;
+export default Header
