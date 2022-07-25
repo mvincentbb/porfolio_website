9589d37fa5de0b876dc4fafabf16679b56e672f1
vincent <vincent.bakpatina@ipnetinstitute.com>
2022-07-25 23:29:43 +0000

    adding project page

---
+++ src/page/project.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default function Project(){
+    return(
+        <div>
+            Project 
+         {/* Quotes */}
+         <h3>Practice, hard work, honesty and team spirit are the ingredients of a correct attitude. Jame Wilson, How to Be Better At Basketball in 21 days</h3>
+         <p>. Jame Wilson, How to Be Better At Basketball in 21 days
+Basketballss</p>
+         <div className="border rounded-lg borger-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+
+
+            </a>
+         </div>
+
+        </div>
+    )
+}
